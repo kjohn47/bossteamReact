ec4271ad2ed7e7db97aad225441df009a7f58db1
Kjohn47 <joaomorgado0@gmail.com>
2019-06-04 15:52:20 +0100

    remove unneeded comment

--- src/ClientApp/store/actions/home/homeServerCalls.tsx
+++ src/ClientApp/store/actions/home/homeServerCalls.tsx
@@ -7,10 +7,6 @@ import axios from 'axios';
 export async function getHomeDataFromServer() : Promise<any>
 {
     ////mocked server data -- replace with server call
-    /*
-    axios.post{}
-    axios.get{}
-    */
     return await serverResolve( () =>
         {            
             return axios.get(restServer + "homePage").then( (response) => {return response.data});
