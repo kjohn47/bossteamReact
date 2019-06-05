fd4b11865b5c6ce097843686bef2fe8f654b20cb
Kjohn47 <joaomorgado0@gmail.com>
2019-06-05 14:58:46 +0100

    Added login verification, added some form fields

--- src/ClientApp/components/Routes/MyAccount.tsx
+++ src/ClientApp/components/Routes/MyAccount.tsx
@@ -2,15 +2,22 @@ import * as React from 'react';
 import {Row, Col} from 'reactstrap';
 import myAccountLogic from '../Logic/Common/MyAccount';
 import MyAccountView from '../View/Common/MyAccount';
+import needLoginLogic from '../Logic/Common/NeedLogin';
+import loginLogic from '../Logic/Common/LoginLogic';
+import Login from '../View/Common/Login';
 
+const LoginForm = loginLogic(Login);
 const MyAccountForm = myAccountLogic( MyAccountView );
+const NeedLogin = needLoginLogic(LoginForm);
 
 class MyAccount extends React.Component{
     render(){
         return(
             <Row>
                 <Col>
-                    <MyAccountForm />
+                    <NeedLogin>
+                        <MyAccountForm />
+                    </NeedLogin>
                 </Col>
             </Row>
         );
